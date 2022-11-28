5af2449ec7a4873d2f32d1cdd38962e27a11b4fe
Officialravi888 <youremail@yourdomain.com>
2022-11-28 16:56:13 +0530

    Added

---
+++ Gen.java
@@ -0,0 +1,10 @@
+public class Gen {
+    public static void show(){
+        System.out.println("hay");
+    }
+
+    public static void main(String[] args) {
+        Gen obj = new Gen();
+        obj.show();
+    }
+}
---
+++ src/main/java/org/example/Gen.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Gen {
+    public static void show(){
+        System.out.println("hay");
+    }
+
+    public static void main(String[] args) {
+        Gen obj = new Gen();
+        obj.show();
+    }
+}
+
